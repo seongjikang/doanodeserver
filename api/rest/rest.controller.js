7b418f377068954fbb6eac56092924cff92bf8b1
문지만 <sh18100309@goldwing.shinhan.com>
2019-08-28 14:14:18 +0900

    aws에 1차 테스트 버전 배포용
    데이터 통신 확인용
    
    refs [issue04]

--- api/rest/rest.controller.js
+++ api/rest/rest.controller.js
@@ -16,7 +16,7 @@ exports.index = (req, res) => {
         value = JSON.stringify(req.body);
     }
 
-    hgetRData("mng_access_token", "user1", function(data) {
+    hgetRData("mng_access_token", "01054103368", function(data) {
         console.log(data);
         // console.log("");
         // console.log(data.access_token);
@@ -25,43 +25,18 @@ exports.index = (req, res) => {
 };
 
 exports.userme = (req, res) => {
-    // req.accepts('application/json');
-
-    // if (req != null) {
-    //     // key = req.body;
-    //     // value = JSON.stringify(req.body);
-    //     // console.log("value = " + value);
-    //     console.log("user = " + req.params);
-    //     console.log("user = " + req.params.user);
-    // }
-
-    // hgetRData("mng_access_token", "user1", function(data) {
-    //     console.log(data);
-    //     // console.log("");
-    //     // console.log(data.access_token);
-    //     res.json(data);
-    // });
-
     const user = parseInt(req.params.user, 10);
     if (!user) {
         console.log("user is none")
         user = "1100035016";
     }
 
-    // let user = users.filter(user => user.id === id)[0]
-    // if (!user) {
-        // return res.status(404).json({error: 'Unknown user'});
-    // }
-
-    // console.log(user);
-
-    // return res.json(user);
-    hgetRData("mng_access_token", "user1", function(data) {
-        // console.log(data);
-        // console.log("");
-        // console.log(data.access_token);
-        // res.json(data);
-        return res.json(getUserMe(data.access_token, user));
+    hgetRData("mng_access_token", "01054103368", function(data) {
+        if (data) {
+            getUserMe(data.access_token, user, res);
+        } else {
+            res.json("data not found");
+        }
     });
 }
 
@@ -98,29 +73,33 @@ exports.callback = (req, res) => {
 };
 
 // 사용자 조회 API
-function getUserMe(access_token, user_seq_no) {
+function getUserMe(access_token, user_seq_no, resp) {
     // console.log('access_token = ' + access_token);
     // console.log('user_seq_no = ' + user_seq_no);
     if (access_token != null && user_seq_no != null) {
-        var getHeader = {
-            "Authorization": "Bearer " + access_token
-        }
+        hgetRData("result_user_me", user_seq_no, function(data) {
+            if (data) {
+                resp.json(data);
+            } else {
+                var getHeader = {
+                    "Authorization": "Bearer " + access_token
+                }
 
-        var option = {
-            headers: getHeader
-            , url: config.ob_url + config.user + "?" + "user_seq_no=" + user_seq_no 
-        }
+                var option = {
+                    headers: getHeader
+                    , url: config.ob_url + config.user + "?" + "user_seq_no=" + user_seq_no 
+                }
 
-        // console.log(option);
-
-        request.get(option, function(err, response, body) {
-            if (err) console.log(err);
-            
-            // console.log(res.statusCode);
-            var json_body = JSON.parse(body);
-            console.log(json_body);
-            return json_body;
-            // hsetRdata("mng_access_token", "user1", JSON.stringify(json_body));
+                request.get(option, function(err, response, body) {
+                    if (err) console.log(err);
+                    
+                    // console.log(res.statusCode);
+                    var json_body = JSON.parse(body);
+                    // console.log(json_body);
+                    hsetRdata("result_user_me", user_seq_no, JSON.stringify(json_body));
+                    resp.json(json_body);
+                });
+            }
         });
     }
 
@@ -152,7 +131,7 @@ function getToken(token_code) {
             
             // console.log(res.statusCode);
             var json_body = JSON.parse(body);
-            hsetRdata("mng_access_token", "user1", JSON.stringify(json_body));
+            hsetRdata("mng_access_token", "01054103368", JSON.stringify(json_body));
         });
     }
 }
@@ -177,11 +156,9 @@ function setRData(key, val) {
 // Redis JSON 데이터 get
 // Key와 Field를 요청하면 조건에 맞는 JSON Data를 Return
 function hgetRData(key, field, callback) {
-    var tField = "obj." + field;
-
-    client.hgetall(key, function(err, obj) {
+    client.hget(key, field, function(err, obj) {
         if (err) console.log(err);
-        var res = JSON.parse(eval(tField));
+        var res = JSON.parse(obj);
         callback(res);
     });
 }
